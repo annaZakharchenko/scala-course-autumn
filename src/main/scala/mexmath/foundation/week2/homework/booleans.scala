9f387376dd1cd1bc00c81e45ce882fab9536b756
annaZakharchenko <anna.zakharchenko@student.karazin.ua>
2024-11-22 14:18:43 +0100

    week-2

--- src/main/scala/mexmath/foundation/week2/homework/booleans.scala
+++ src/main/scala/mexmath/foundation/week2/homework/booleans.scala
@@ -9,34 +9,76 @@ object booleans:
     def substitute(variable: Variable, expression: Expression): Expression
 
   sealed trait Boolean extends Expression:
-    val evaluate: Expression                                               = this
-    def substitute(variable: Variable, expression: Expression): Expression = ???
+    def evaluate: Expression
+    def substitute(variable: Variable, expression: Expression): Expression
 
-  type True = True.type
-  case object True extends Boolean
+  case object True extends Boolean:
+    def evaluate: Expression                                               = this
+    def substitute(variable: Variable, expression: Expression): Expression = this
 
-  type False = False.type
-  case object False extends Boolean
+  case object False extends Boolean:
+    def evaluate: Expression                                               = this
+    def substitute(variable: Variable, expression: Expression): Expression = this
 
-  type Variable
+  case class Variable(name: String) extends Expression:
+    def evaluate: Expression = this
+
+    def substitute(variable: Variable, expression: Expression): Expression =
+      if this == variable then expression
+      else this
+
+    override def toString: String = name
 
   case class Negation(expression: Expression) extends Expression:
 
-    def evaluate: Expression                                               = ???
-    def substitute(variable: Variable, expression: Expression): Expression = ???
+    def evaluate: Expression = expression.evaluate match {
+      case True  => False
+      case False => True
+      case expr  => Negation(expr)
+    }
 
-    override def toString: String = ???
+    def substitute(variable: Variable, expression: Expression): Expression =
+      Negation(this.expression.substitute(variable, expression))
 
-  // Provide implementation for `Conjunction` type
-  type Conjunction
+    override def toString: String = s"!(${expression.toString})"
 
-  // Provide implementation for `Disjunction` type
-  type Disjunction
+  case class Conjunction(left: Expression, right: Expression) extends Expression:
 
-  // Provide implementation for `Implication` type
-  type Implication
+    def evaluate: Expression = (left.evaluate, right.evaluate) match {
+      case (True, True) => True
+      case _            => False
+    }
 
-  // Add conversion from `String` to `Variable`
+    def substitute(variable: Variable, expression: Expression): Expression =
+      Conjunction(left.substitute(variable, expression), right.substitute(variable, expression))
+
+    override def toString: String = s"(${left.toString} ∧ ${right.toString})"
+
+  case class Disjunction(left: Expression, right: Expression) extends Expression:
+
+    def evaluate: Expression =
+      if left.evaluate == True then True
+      else right.evaluate
+
+    def substitute(variable: Variable, expression: Expression): Expression =
+      Disjunction(left.substitute(variable, expression), right.substitute(variable, expression))
+
+    override def toString: String = s"(${left.toString} ∨ ${right.toString})"
+
+  case class Implication(left: Expression, right: Expression) extends Expression:
+
+    def evaluate: Expression = (left.evaluate, right.evaluate) match {
+      case (True, False) => False
+      case _             => True
+    }
+
+    def substitute(variable: Variable, expression: Expression): Expression =
+      Implication(left.substitute(variable, expression), right.substitute(variable, expression))
+
+    override def toString: String = s"(${left.toString} → ${right.toString})"
+
+  given Conversion[String, Variable] with
+    def apply(str: String): Variable = Variable(str)
 
   extension (expr: Expression)
 
@@ -44,10 +86,10 @@ object booleans:
     infix def unary_! : Negation = Negation(expr)
 
     @targetName("Conjunction")
-    infix def ∧(that: Expression): Conjunction = ???
+    infix def ∧(that: Expression): Conjunction = Conjunction(expr, that)
 
     @targetName("Disjunction")
-    infix def ∨(that: Expression): Disjunction = ???
+    infix def ∨(that: Expression): Disjunction = Disjunction(expr, that)
 
     @targetName("Implication")
-    infix def →(that: Expression): Implication = ???
+    infix def →(that: Expression): Implication = Implication(expr, that)
--- src/test/scala/mexmath/foundation/week2/homework/arbitraries.scala
+++ src/test/scala/mexmath/foundation/week2/homework/arbitraries.scala
@@ -8,22 +8,34 @@ object arbitraries:
 
   val genBoolean: Gen[Boolean] = Gen.oneOf(True, False)
 
-  val genVariableName: Gen[String] = ???
+  val genVariableName: Gen[String] = Gen.alphaStr.suchThat(_.nonEmpty)
 
-  val genVariable: Gen[Variable] = ???
+  val genVariable: Gen[Variable] =
+    for name <- genVariableName
+    yield Variable(name)
 
   val genNegation: Gen[Negation] =
-    for expression <- genExpression
-      yield Negation(expression)
+    for expression <- lzy(genExpression)
+    yield Negation(expression)
 
-  val genConjunction: Gen[Conjunction] = ???
+  val genConjunction: Gen[Conjunction] = for
+    left  <- lzy(genExpression)
+    right <- lzy(genExpression)
+  yield Conjunction(left, right)
 
-  val genDisjunction: Gen[Disjunction] = ???
+  val genDisjunction: Gen[Disjunction] = for
+    left  <- lzy(genExpression)
+    right <- lzy(genExpression)
+  yield Disjunction(left, right)
 
-  val genImplication: Gen[Implication] = ???
+  val genImplication: Gen[Implication] = for
+    left  <- lzy(genExpression)
+    right <- lzy(genExpression)
+  yield Implication(left, right)
 
-  lazy val genExpression: Gen[Expression] = ???
+  lazy val genExpression: Gen[Expression] =
+    Gen.frequency((1, genBoolean), (1, genVariable), (1, genNegation), (1, genConjunction), (1, genDisjunction), (1, genImplication))
 
-  given Arbitrary[Boolean]    = ???
-  given Arbitrary[Variable]   = ???
-  given Arbitrary[Expression] = ???
+  given Arbitrary[Boolean]    = Arbitrary(genBoolean)
+  given Arbitrary[Variable]   = Arbitrary(genVariable)
+  given Arbitrary[Expression] = Arbitrary(genExpression)
